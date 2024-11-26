7d6c5d26f038555cd953ca54cd62e26af925936e
Rodrigo Saadi <61701240+rsaadi28@users.noreply.github.com>
2024-11-26 19:02:10 +0000

    add `.editorconfig` file and prettier scripts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
-    return (
-        <h1>Calculadora de latas de Tinta para uma sala</h1>
-    );
+  return (
+    <h1>Calculadora de latas de Tinta para uma sala</h1>
+  );
 }
 
 export default Home;
